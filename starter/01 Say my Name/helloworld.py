aff3504fd7035f52139308b220a4515fba00d76f
guille <tegonza@microsoft.com>
2024-04-19 14:49:52 -0600

    Add hello_world function to helloworld.py

---
+++ starter/01 Say my Name/helloworld.py
@@ -0,0 +1,9 @@
+# create a hello world function that recieves a name parameter and test that it works 
+
+def hello_world(name):
+    print(f"Hello, {name}!")
+
+# Testing the function
+hello_world("Alice")
+hello_world("Bob")
+hello_world("Charlie")
